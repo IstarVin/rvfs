6a5888fe91a2c04a4369014f049430621ad3dd90
IstarVin <ajdeleverio20@gmail.com>
2026-03-11 22:24:34 +0800

    init

---
+++ internal/cli/root.go
@@ -0,0 +1,13 @@
+package cli
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "rvfs",
+	Short: "An offline-first FUSE filesystem",
+}
+
+// Execute runs the root command. Called from main.
+func Execute() error {
+	return rootCmd.Execute()
+}
